dd9193f5c868e00764588502fd5f412af071a9fe
Vincent Beauregard <vincent.beauregard@usherbrooke.ca>
2021-12-02 12:53:45 -0500

    🎨 Changed `api.observations` returned schema
    to match previous `observations` table and facilitate migration

--- scripts/API_observations_view.sql
+++ scripts/API_observations_view.sql
@@ -1,14 +1,15 @@
 DROP VIEW IF EXISTS api.observations CASCADE;
 CREATE VIEW api.observations AS (
 select
-		st_astext(obs.geom),
-		obs.year_obs as year,
-		obs.month_obs as month,
-		obs.day_obs as day,
-		obs.time_obs as time,
+		st_astext(obs.geom) as geom_wkt,
+		obs.year_obs,
+		obs.month_obs,
+		obs.day_obs,
+		obs.time_obs,
 		obs.obs_value as value,
 		var.name as variable,
 		var.unit as variable_unit,
+		taxa.id_taxa_obs,
 		taxa.observed_scientific_name taxa_observed_scientific_name,
 		taxa.valid_scientific_name taxa_valid_scientific_name,
 		taxa.vernacular_en taxa_vernacular_en,
@@ -19,6 +20,7 @@ select
 		taxa.source_references taxa_reference_sources,
 		ds.type_sampling,
 		ds.type_obs,
+		ds.id as id_datasets,
 		ds.title as dataset,
 		ds.original_source as source,
 		obs.org_id_obs as source_record_id
