2ec88e27ac96d71aad78bceecd064635bfcd5907
Vincent Beauregard <vincent.beauregard@usherbrooke.ca>
2021-12-02 17:00:07 -0500

    🎨 Modified `api.observations` for compatibility

--- scripts/API_observations_view.sql
+++ scripts/API_observations_view.sql
@@ -1,7 +1,8 @@
 DROP VIEW IF EXISTS api.observations CASCADE;
 CREATE VIEW api.observations AS (
-select
-		st_astext(obs.geom) as geom_wkt,
+	select
+		obs.id,
+		obs.geom,
 		obs.year_obs,
 		obs.month_obs,
 		obs.day_obs,
@@ -9,7 +10,7 @@ select
 		obs.obs_value as value,
 		var.name as variable,
 		var.unit as variable_unit,
-		taxa.id_taxa_obs,
+		obs.id_taxa_obs,
 		taxa.observed_scientific_name taxa_observed_scientific_name,
 		taxa.valid_scientific_name taxa_valid_scientific_name,
 		taxa.vernacular_en taxa_vernacular_en,
@@ -20,7 +21,7 @@ select
 		taxa.source_references taxa_reference_sources,
 		ds.type_sampling,
 		ds.type_obs,
-		ds.id as id_datasets,
+		obs.id_datasets,
 		ds.title as dataset,
 		ds.original_source as source,
 		obs.org_id_obs as source_record_id
