089ab0df5b939319a4b188684668fe7e92a7027e
Vincent Beauregard <vincent.beauregard@usherbrooke.ca>
2021-12-08 17:57:32 -0500

    💡 Updated dump files

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM postgres:13.1
 
 RUN apt update && apt install postgis postgresql-13-postgis-3 postgresql-plpython3-13 python3-setuptools python3-pip git -y
 
-RUN pip3 install git+https://github.com/ReseauBiodiversiteQuebec/bdqc_taxa.git#egg=bdqc_taxa
-
 RUN git config --global user.email "vincent.beauregard@usherbrooke.ca"
 RUN git config --global user.name "Vincent Beauregard"
 
-COPY ./atlas.sql /docker-entrypoint-initdb.d/atlas.sql
-# COPY ./copy_test_observations.sql /docker-entrypoint-initdb.d/copy_test_observations.sql
+RUN pip3 install git+https://github.com/ReseauBiodiversiteQuebec/bdqc_taxa.git#egg=bdqc_taxa
+
+COPY ./dump_atlas.sql /docker-entrypoint-initdb.d/0_dump_atlas.sql
+COPY ./dump_test_observations.sql /docker-entrypoint-initdb.d/1_dump_test_observations.sql
+COPY ./dump_hex.sql /docker-entrypoint-initdb.d/2_dump_hex.sql
+
--- make_dump.sh
+++ make_dump.sh
@@ -1,6 +1,8 @@
 export PGDATABASE="atlas"
 
-out_file="atlas.sql"
+out_file="dump_atlas.sql"
+hex_file="dump_hex.sql"
+obs_file="dump_test_observations.sql"
 
 # Dump users and roles
 pg_dumpall --roles-only  > $out_file
@@ -11,7 +13,24 @@ grep -v "ALTER ROLE postgres" $out_file > tmpfile && mv tmpfile $out_file
 pg_dump -s >>$out_file
 pg_dump -a \
     -T observations \
-    -T obs_efforts >>$out_file
+    -T obs_efforts \
+    -T public_api.hex_250_na \
+    -T public_api.hexquebec100km \
+    -T public_api.hexquebec10km  \
+    -T public_api.hexquebec20km  \
+    -T public_api.hexquebec50km  \
+    -T public_api.hexquebec5km   \
+    >>$out_file
+
+# DUMP hex data in a separate file for space under 100 Mb git management
+pg_dump -a \
+    -t public_api.hex_250_na     \
+    -t public_api.hexquebec100km \
+    -t public_api.hexquebec10km  \
+    -t public_api.hexquebec20km  \
+    -t public_api.hexquebec50km  \
+    -t public_api.hexquebec5km   \
+    >$hex_file
 
 # Dump selected observations in 'test_observations.txt' file and add line to copy in sql dump
 psql -c 'create table public.test_observations (like public.observations including all);
@@ -22,11 +41,11 @@ from public.observations
 order by random()
 limit 60000;'
 
-echo "COPY public.observations FROM stdin;">>$out_file
-psql -c 'COPY test_observations TO stdout'>>$out_file
-echo "\.">>$out_file
+echo "COPY public.observations FROM stdin;">$obs_file
+psql -c 'COPY test_observations TO stdout'>>$obs_file
+echo "\.">>$obs_file
 
-echo "COPY public.obs_efforts FROM stdin;">>$out_file
+echo "COPY public.obs_efforts FROM stdin;">>$obs_file
 psql -c '
 COPY (
     SELECT *
@@ -34,7 +53,7 @@ COPY (
     WHERE id_obs IN (
         SELECT obs.id
         FROM test_observations obs))
-TO stdout'>>$out_file
-echo "\.">>$out_file
+TO stdout'>>$obs_file
+echo "\.">>$obs_file
 
 psql -c "drop table test_observations;"
