cbcc4fead64902085d467ab7a1721d421755ccd6
Vincent Beauregard <vincent.beauregard@usherbrooke.ca>
2021-12-09 14:27:00 -0500

    ⚡️ Added `within_quebec` column to observations
    For faster indexing of observations within Quebec
    Also implemented trigger for automated set on insertion

--- make_dump.sh
+++ make_dump.sh
@@ -16,6 +16,7 @@ pg_dump -a \
     -T observations \
     -T obs_efforts \
     -T public_api.hex_250_na \
+    -T public_api.hexquebec      \
     -T public_api.hexquebec100km \
     -T public_api.hexquebec10km  \
     -T public_api.hexquebec20km  \
@@ -28,36 +29,32 @@ echo "SET session_replication_role = 'replica';" > $hex_file
 
 pg_dump -a \
     -t public_api.hex_250_na     \
-    -t public_api.hexquebec100km \
-    -t public_api.hexquebec10km  \
-    -t public_api.hexquebec20km  \
-    -t public_api.hexquebec50km  \
-    -t public_api.hexquebec5km   \
+    -t public_api.hexquebec      \
     >> $hex_file
 
 # Dump selected observations in 'test_observations.txt' file and add line to copy in sql dump
-echo "SET session_replication_role = 'replica';" > $obs_file 
-psql -c 'create table public.test_observations (like public.observations including all);
+# echo "SET session_replication_role = 'replica';" > $obs_file 
+# psql -c 'create table public.test_observations (like public.observations including all);
 
-insert into public.test_observations
-select *
-from public.observations
-order by random()
-limit 60000;'
+# insert into public.test_observations
+# select *
+# from public.observations
+# order by random()
+# limit 60000;'
 
-echo "COPY public.observations FROM stdin;">>$obs_file
-psql -c 'COPY test_observations TO stdout'>>$obs_file
-echo "\.">>$obs_file
+# echo "COPY public.observations FROM stdin;">>$obs_file
+# psql -c 'COPY test_observations TO stdout'>>$obs_file
+# echo "\.">>$obs_file
 
-echo "COPY public.obs_efforts FROM stdin;">>$obs_file
-psql -c '
-COPY (
-    SELECT *
-    FROM obs_efforts
-    WHERE id_obs IN (
-        SELECT obs.id
-        FROM test_observations obs))
-TO stdout'>>$obs_file
-echo "\.">>$obs_file
+# echo "COPY public.obs_efforts FROM stdin;">>$obs_file
+# psql -c '
+# COPY (
+#     SELECT *
+#     FROM obs_efforts
+#     WHERE id_obs IN (
+#         SELECT obs.id
+#         FROM test_observations obs))
+# TO stdout'>>$obs_file
+# echo "\.">>$obs_file
 
-psql -c "drop table test_observations;"
+# psql -c "drop table test_observations;"
