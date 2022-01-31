a3c9d1ff655118c2b81227cbae4d5d5a686cfc4d
Vincent Beauregard <vincent.beauregard@usherbrooke.ca>
2022-01-31 14:53:17 -0500

    ✨ Added `api.taxa_groups` endpoint

---
+++ scripts/API_taxa_groups.sql
@@ -0,0 +1,20 @@
+DROP VIEW IF EXISTS api.taxa_groups CASCADE;
+CREATE VIEW api.taxa_groups AS (
+    with taxa_obs_agg as (
+        select
+            id_group,
+            array_agg(id_taxa_obs) id_taxa_obs
+        from taxa_obs_group_lookup
+        where id_taxa_obs is not null
+        group by id_group
+    )
+    select
+        taxa_groups.id,
+        taxa_groups.vernacular_en,
+        taxa_groups.vernacular_fr,
+        taxa_obs_agg.id_taxa_obs
+    from
+        taxa_groups
+    left join taxa_obs_agg on taxa_groups.id = taxa_obs_agg.id_group
+    where taxa_groups.level = 1
+)
