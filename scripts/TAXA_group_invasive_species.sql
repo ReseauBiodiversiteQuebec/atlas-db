57371dff1309ede558047b415eed9ed53930ef00
Vincent Beauregard <vincent.beauregard@usherbrooke.ca>
2022-09-20 18:03:33 -0400

    ✨ Invasive species definition

---
+++ scripts/TAXA_group_invasive_species.sql
@@ -0,0 +1,79 @@
+INSERT INTO taxa_group (vernacular_fr, vernacular_en, level)
+VALUES ('Espèces envahissantes', 'Invasive species', 2);
+
+
+DO
+$$
+DECLARE
+    id_group integer;
+BEGIN;
+SELECT id 
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Harmonia axyridis', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Anoplophora glabripennis', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Popillia japonica', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Bythotrephes longimanus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Eriocheir sinensis', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Hemimysis anomala', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Orconectes rusticus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Dreissena bugensis', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Dreissena polymorpha', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Potamopyrgus antipodarum', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Corbicula fluminea', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Echinogammarus ischnus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Bellamya chinensis', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Cervus elaphus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Cygnus olor', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Ctenopharyngodon idella', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Lepomis cyanellus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Pseudorasbora parva', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Scardinius erythrophthalmus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Neogobius melanostomus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Carassius auratus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Tinca tinca', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Trachemys scripta elegans', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Channa', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Parachanna', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Trapa natans', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Nymphoides peltata', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Hydrocharis morsus-ranae', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Eichhornia crassipes', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Pistia stratiotes', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Salvinia', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Cabomba caroliniana', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Brazilian waterweed', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Hydrilla verticillata', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Myriophyllum aquaticum', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Myriophyllum spicatum', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Potamogeton crispus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Phalaris arundinacea', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Butomus umbellatus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Glyceria maxima', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Iris pseudacorus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Rorippa amphibia', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Phragmites australis australis', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Lythrum salicaria', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Stratiotes aloides', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Alliaria petiolata', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Anthriscus sylvestris', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Heracleum sphondylium', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Heracleum mantegazzianum', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Symphytum officinale', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Vincetoxicum rossicum', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Vincetoxicum nigrum', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Aegopodium podagraria', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Acer platanoides', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Acer negundo', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Galium mollugo', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Impatiens glandulifera', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Miscanthus sacchariflorus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Frangula alnus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Rhamnus carthartica', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Ulmus pumila', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Pastinaca sativa', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Petasites japonicus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Reynoutria xbohemica', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Reynoutria sachalinensis', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Reynoutria japonica', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Helianthus tuberosus', 25);
+INSERT INTO public.taxa_group_members ("scientific_name", "id_group") VALUES ('Valeriana officinalis', 25);
+COMMIT;
