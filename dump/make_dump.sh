0f1865be130b11cdd1cb3496375ca0e8861717b9
Vincent Beauregard <vincent.beauregard@usherbrooke.ca>
2023-07-05 16:13:16 -0400

    ♻️ Make dump update

--- dump/make_dump.sh
+++ dump/make_dump.sh
@@ -1,8 +1,6 @@
 export PGDATABASE="atlas"
 
 out_file="dump_atlas.sql"
-hex_file="dump_hex.sql"
-ecozones_file="dump_ecozones.sql"
 obs_file="dump_test_observations.sql"
 
 # Dump users and roles
@@ -13,9 +11,10 @@ pg_dumpall --roles-only >> $out_file
 grep -v "CREATE ROLE postgres" $out_file > tmpfile && mv tmpfile $out_file
 grep -v "ALTER ROLE postgres" $out_file > tmpfile && mv tmpfile $out_file
 
+# Dump using -s to dump only schema and -a to dump only data
 pg_dump -s --no-tablespaces \
     >> $out_file
-pg_dump -a \
+pg_dump -a --if-exists\
     -T observations \
     -T obs_efforts \
     -T public.observations_backup_202111 \
@@ -26,21 +25,6 @@ pg_dump -a \
     -N scratch_vbeaure   \
     -N observations_partitions \
     >> $out_file
-
-# DUMP hex data in a separate file for space under 100 Mb git management
-echo "SET session_replication_role = 'replica';" > $hex_file 
-
-pg_dump -c --if-exists\
-    -t public_api.hex_250_na     \
-    -t public_api.hexquebec      \
-    >> $hex_file
-
-
-echo "SET session_replication_role = 'replica';" > $ecozones_file 
-
-pg_dump -c --if-exists\
-    -t public_api.cadre_eco_quebec     \
-    >> $ecozones_file
 
 # Dump selected observations in 'test_observations.txt' file and add line to copy in sql dump
 echo "SET session_replication_role = 'replica';" > $obs_file 
