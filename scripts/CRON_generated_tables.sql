5c78b32a5e92e837b2747ab5211108a911a19be1
Vincent Beauregard <vincent.beauregard@usherbrooke.ca>
2023-08-22 21:05:53 -0400

    🐛 Fixed generated tables refresh

--- scripts/CRON_generated_tables.sql
+++ scripts/CRON_generated_tables.sql
@@ -6,32 +6,43 @@
 -- - table named atlas_api.generated_tables_metadata that contains the list of generated tables and related metadata to update them
 -- - function named atlas_api.update_generated_tables that updates all generated tables and related metadata
 
+DROP TABLE IF EXISTS generated_tables_metadata CASCADE;
 CREATE TABLE generated_tables_metadata (
     id SERIAL PRIMARY KEY,
     source_table_name TEXT NOT NULL,
-    source_table_last_modified_column TEXT NOT NULL DEFAULT 'modified_at',
+    source_table_last_modified_column TEXT NOT NULL,
     source_table_last_modified TIMESTAMP,
     generated_table_name TEXT NOT NULL,
-    generated_table_last_update TIMESTAMP NOT NULL,
+    generated_table_last_update TIMESTAMP DEFAULT '1970-01-01 00:00:00',
     update_statement TEXT NOT NULL,
     status TEXT,
     return_message TEXT
 );
 
+-- Unique constraint on generated_table_name, source_table_name and update_statement
+CREATE UNIQUE INDEX generated_tables_metadata_unique_idx ON generated_tables_metadata (generated_table_name, source_table_name, update_statement);
+
 CREATE OR REPLACE FUNCTION update_generated_tables_metadata()
 RETURNS void AS $$
 DECLARE
     generated_table RECORD;
-    source_table TEXT;
+    source_table RECORD;
     update_statement TEXT;
     return_message TEXT;
 BEGIN
     -- Update source tables last modified date
     FOR source_table IN
-        SELECT DISTINCT source_table_name
+        SELECT DISTINCT source_table_name, source_table_last_modified_column
         FROM generated_tables_metadata
     LOOP
-        EXECUTE format('UPDATE generated_tables_metadata SET source_table_last_modified = (SELECT MAX(%I) FROM %I) WHERE source_table_name = %L', source_table.source_table_last_modified_column, source_table.source_table_name, source_table.source_table_name);
+        EXECUTE format(
+            'UPDATE generated_tables_metadata
+            SET source_table_last_modified = (SELECT MAX(%I) FROM %s)
+            WHERE source_table_name = %L',
+            source_table.source_table_last_modified_column,
+            source_table.source_table_name,
+            source_table.source_table_name
+        );
     END LOOP;
 
     -- Update generated tables and last update date
@@ -64,6 +75,14 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+SELECT update_generated_tables_metadata();
+
 -- INSERT GENERATED TABLES METADATA
 INSERT INTO generated_tables_metadata (generated_table_name, update_statement, source_table_name, source_table_last_modified_column)
-VALUES ('atlas_api.web_regions', 'REFRESH MATERIALIZED VIEW CONCURRENTLY atlas_api.web_regions;', 'public.regions', 'updated_at');
+VALUES
+    ('atlas_api.web_regions', 'REFRESH MATERIALIZED VIEW CONCURRENTLY atlas_api.web_regions;', 'public.regions', 'updated_at'),
+    ('atlas_api.obs_region_counts', 'SELECT atlas_api.obs_region_counts_refresh()', 'public.observations', 'modified_at'),
+    ('atlas_api.obs_region_counts', 'SELECT atlas_api.obs_region_counts_refresh()', 'public.regions', 'updated_at'),
+    ('atlas_api.obs_regions_taxa_datasets_counts', 'SELECT obs_regions_taxa_datasets_counts_refresh()', 'public.observations', 'modified_at'),
+    ('atlas_api.obs_regions_taxa_datasets_counts', 'SELECT obs_regions_taxa_datasets_counts_refresh()', 'public.observations', 'modified_at')
+ON CONFLICT DO NOTHING;
